793f2aca3ebab7d673763396462ff92827db1cbd
Pete Birkinshaw <pete@binary-ape.org>
2013-11-20 23:07:33 +0000

    More idle confused poking at things I don't understand

--- src/main/java/ShibShim.java
+++ src/main/java/ShibShim.java
@@ -1,12 +1,17 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.util.UUID;
+
 
 
 public class ShibShim extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
             throws ServletException, IOException {
+
+        String uuid = UUID.randomUUID().toString();
+        //System.out.println("uuid = " + uuid);
 
         PrintWriter out = response.getWriter();
         out.println( "Hello World!" );
