60329ddba23c9e6615837fe0a434fb3680e7ad27
Pete Birkinshaw <pete@binary-ape.org>
2013-11-21 22:11:45 +0000

    Logging works, I think.

--- src/main/java/ShabtiShim.java
+++ src/main/java/ShabtiShim.java
@@ -2,17 +2,24 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.UUID;
-
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
 
 
 public class ShabtiShim extends HttpServlet {
+
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
             throws ServletException, IOException {
 
+        BasicConfigurator.configure();
+        Logger logger = Logger.getLogger("Shabti.Shim");
+
         String uuid = UUID.randomUUID().toString();
 
         String token = request.getPathInfo();
+
+        logger.debug(uuid);
 
         PrintWriter out = response.getWriter();
         out.println( "Hello!" );
@@ -21,4 +28,5 @@ public class ShabtiShim extends HttpServlet {
         out.flush();
         out.close();
     }
+
 }
