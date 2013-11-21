d662ec141cf3c47ee047a89284eb92fd5d9de7ea
Pete Birkinshaw <pete@binary-ape.org>
2013-11-21 22:49:21 +0000

    Can tell if request should be incoming or outgoing

--- src/main/java/ShabtiShim.java
+++ src/main/java/ShabtiShim.java
@@ -15,13 +15,35 @@ public class ShabtiShim extends HttpServlet {
         BasicConfigurator.configure();
         Logger logger = Logger.getLogger("Shabti.Shim");
 
+        PrintWriter out = response.getWriter();
+
         String uuid = UUID.randomUUID().toString();
 
         String token = request.getPathInfo();
 
+        // We are not interested in a lone slash or a leading slash.
+        token = token.startsWith("/") ? token.substring(1) : token;
+
         logger.debug(uuid);
 
-        PrintWriter out = response.getWriter();
+        // No token in path means we need to create one and redirect to external authenticator
+        if (token == null || token.isEmpty() ) {
+
+            out.println( "Outgoing!" );
+            logger.debug("Outgoing! (redirecting out)");
+
+
+        }
+
+        // A token, so we should check it returns valid authenticated data
+        else {
+
+            out.println( "Incoming!" );
+            logger.debug("Incoming!");
+            logger.debug(token);
+
+        }
+
         out.println( "Hello!" );
         out.println( uuid );
         out.println( token );
