667d45428418f2513bcb8dd6e5e81d9da75b4fc8
AvengerPenguin <github@rossfenning.co.uk>
2016-07-24 18:05:24 +0100

    For fun, let the tester choose a principal name

--- sandbox/auth/auth.py
+++ sandbox/auth/auth.py
@@ -12,8 +12,9 @@ r = redis.StrictRedis(host='redis', port=6379, db=0)
 @app.route("/", methods=['POST'])
 def login():
     token = request.form['token']
+    principal = request.form['principal']
     demand = json.loads(r.get(token))
-    demand['principal'] = 'test_mctestface'
+    demand['principal'] = principal
     r.set(token, json.dumps(demand))
     # There's definitely a better way to build a URL safely :-)
     return redirect("https://shib.local:4443/idp/Authn/Shim/Return?token=" + token, code=302)
@@ -25,6 +26,8 @@ def auth(token):
     return """
     <p>Welcome to auth:<p>
     <form action="/" method="post">
+    Please choose a principal name:
+    <input type="text" name="principal" value="testy_mctestface"/><br>
     <input type="submit" name="login" value="login"/>
     <input type="hidden" name="token" value="{}"/>
     </form>
