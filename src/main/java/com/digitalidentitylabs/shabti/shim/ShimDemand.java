839ac3e642f4f38db34ea70be9046fffedba033f
Pete Birkinshaw <pete@binary-ape.org>
2017-03-23 16:58:24 +0000

    Reformat things a little with whitespace and love

--- src/main/java/com/digitalidentitylabs/shabti/shim/ShimDemand.java
+++ src/main/java/com/digitalidentitylabs/shabti/shim/ShimDemand.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ShimDemand {
 
-    @JsonProperty("external_auth_key")        protected String  externalAuthKey = "";
-    @JsonProperty("force")         protected Boolean forceAuthn = false;
-    @JsonProperty("passive")       protected Boolean isPassive = false;
-    @JsonProperty("method")        protected String  authnMethod = "";
-    @JsonProperty("relying_party") protected String  relyingParty = "";
-    @JsonProperty("principal")        protected String  principal = "";
+    @JsonProperty("external_auth_key") protected String  externalAuthKey = "";
+    @JsonProperty("force")             protected Boolean forceAuthn = false;
+    @JsonProperty("passive")           protected Boolean isPassive = false;
+    @JsonProperty("method")            protected String  authnMethod = "";
+    @JsonProperty("relying_party")     protected String  relyingParty = "";
+    @JsonProperty("principal")         protected String  principal = "";
 }
